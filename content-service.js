8bd109a3dd3093625f79c6adb64854253c5a86e8
nbist24k <nbist291@gmail.com>
2024-10-04 01:24:51 -0400

    AS2 P2S4

--- content-service.js
+++ content-service.js
@@ -1,27 +1,84 @@
+// require the fs module
+const fs = require("fs");
+
+// require the path module
+const path = require("path");
+
 //Global arrays to store the data
 let articles = [];
 let categories = [];
 
-//Placeholder for the initialize() function
+//Initialize the data by reading JSON files
 function initialize() {
   return new Promise((resolve, reject) => {
-    resolve();
+    fs.readFile(
+      path.join(__dirname, "data/articles.json"),
+      "utf8",
+      (err, data) => {
+        if (err) {
+          reject("Unable to read articles file");
+          return;
+        }
+        articles = JSON.parse(data);
+
+        fs.readFile(
+          path.join(__dirname, "data/categories.json"),
+          "utf8",
+          (err, data) => {
+            if (err) {
+              reject("Unable to read categories file");
+              return;
+            }
+            categories = JSON.parse(data);
+            resolve();
+          }
+        );
+      }
+    );
   });
 }
 
-//Placeholder for the getPublishedArticles() function
+//Get published articles (only those with `published` set to true)
 function getPublishedArticles() {
   return new Promise((resolve, reject) => {
-    resolve();
+    const publishedArticles = articles.filter(
+      (article) => article.published === true
+    );
+    if (publishedArticles.length > 0) {
+      resolve(publishedArticles);
+    } else {
+      reject("No published articles available");
+    }
   });
 }
 
-//Placeholder for the getCategories() function
+//Get all articles
+function getAllArticles() {
+  return new Promise((resolve, reject) => {
+    if (articles.length > 0) {
+      resolve(articles);
+    } else {
+      reject("No articles available");
+    }
+  });
+}
+
+//Get all categories
 function getCategories() {
   return new Promise((resolve, reject) => {
+    if (categories.length > 0) {
+      resolve(categories);
+    } else {
+      reject("No categories available");
+    }
     resolve();
   });
 }
 
 //Export the functions
-module.exports = { initialize, getPublishedArticles, getCategories };
+module.exports = {
+  initialize,
+  getPublishedArticles,
+  getAllArticles,
+  getCategories,
+};
